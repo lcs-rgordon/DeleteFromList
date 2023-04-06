64db80426f1be038327013de160a80fcc4d69525
Russell Gordon <rgordon@lcs.on.ca>
2023-04-06 05:33:41 -0400

    Initial Commit

---
+++ DeleteFromList/DeleteFromListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteFromListApp.swift
+//  DeleteFromList
+//
+//  Created by Russell Gordon on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct DeleteFromListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
